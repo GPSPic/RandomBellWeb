1c96dfcfb64b97794662634290de37330df5b3ea
GPSPic <picard.guillaume47@gmail.com>
2023-09-29 15:46:01 +0100

    added sound effect to bell button onClick

--- src/components/bell/Bell.js
+++ src/components/bell/Bell.js
@@ -1,17 +1,22 @@
 import { useContext } from "react";
+import useSound from "use-sound";
+
 import { ThemeContext } from "../../containers/Container";
-import RegularText from "../texts/RegularText";
-import "./Bell.css"
-import { bellDark, bellLight } from "../../helpers/icon"
+import "./Bell.css";
+import { bellDark, bellLight } from "../../helpers/icon";
+import bellChime from "../../helpers/sounds/happy-bell.mp3";
 
 const Bell = () => {
+  const [play] = useSound(bellChime);
+
   const theme = useContext(ThemeContext);
-  const bellButtonTheme = "bell-button-" + theme 
+  const bellButtonTheme = "bell-button-" + theme;
 
   return (
     <div className="bell-box">
-      <button className={`${bellButtonTheme} bell-button`}>
-            {theme === 'dark' ? bellDark : bellLight}
+      <button className={`${bellButtonTheme} bell-button`} onClick={play}>
+        {theme === "dark" ? bellDark : bellLight}
+        {/* <img scr="../../helpers/icons8-bell.gif"/> */}
       </button>
     </div>
   );
