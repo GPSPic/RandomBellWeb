ffc159839828fbb6adb999ab4c5b7428777725de
GPSPic <picard.guillaume47@gmail.com>
2023-10-02 16:30:51 +0100

    spacing

--- src/components/SetRoutine/MinInterval.js
+++ src/components/SetRoutine/MinInterval.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import RegularText from "../texts/RegularText.js";
-import "./SetRoutine.css"
-import "../../helpers/colours.css"
+
+import "./SetRoutine.css";
+import "../../helpers/colours.css";
+
 const MinInterval = ({ minInterval, handleMinIntChange }) => {
   const [newMinInterval, setNewMinInterval] = useState(minInterval);
+
   const handleChange = (e) => {
     setNewMinInterval(e.target.value);
     handleMinIntChange(e.target.value);
   };
+  
   return (
     <div className="min-interval">
       <RegularText text="Minimum Interval" />
-      <input type="time" value={newMinInterval} onChange={handleChange}/>
+      <input type="time" value={newMinInterval} onChange={handleChange} />
     </div>
   );
 };
